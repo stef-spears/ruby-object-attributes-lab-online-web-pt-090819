2da2f46b9841635450c8e1c63e915028099a5421
Stef Spears <spears.sj87@gmail.com>
2019-09-25 21:48:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,19 @@
 class Dog 
   
   def name=(dog_name)
-    @this_dog = dog_name
+    @this_dog_name = dog_name
   end 
   
   def breed=(dog_breed)
-    
+    @this_dog_breed = dog_breed
   end
   
   def name 
-    @name
+    @this_dog_name
   end 
   
   def breed 
-    @breed
+    @this_dog_breed
   end 
   
 end
